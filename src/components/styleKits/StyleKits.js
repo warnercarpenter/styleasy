421fae7793d0135591a2edd73f079d43f2b84e9d
Warner Carpenter <warnercarpenter@yahoo.com>
2019-04-09 18:30:18 -0500

    decided to keep login but remove passwords. Warning message displays on login screen

--- src/components/styleKits/StyleKits.js
+++ src/components/styleKits/StyleKits.js
@@ -11,16 +11,22 @@ class StyleKits extends Component {
 
     render() {
         return (
-            <section className="styleKits">
-                {
-                    this.props.styleKits.filter(currentKit => currentKit.userId === parseInt(sessionStorage.getItem("credentials")))
-                    .map(currentKit => {
-                            return (
-                                <KitCard setPathname={this.props.setPathname} editKitName={this.props.editKitName} history={this.props.history} deleteKit={this.props.deleteKit} key={currentKit.id} kit={currentKit} />
-                            )
-                        })
-                }
-            </section>
+            (this.props.styleKits.filter(currentKit => currentKit.userId === parseInt(sessionStorage.getItem("credentials"))).length > 0) ? (
+                <section className="styleKits">
+                    {
+                        this.props.styleKits.filter(currentKit => currentKit.userId === parseInt(sessionStorage.getItem("credentials")))
+                            .map(currentKit => {
+                                return (
+                                    <KitCard setPathname={this.props.setPathname} editKitName={this.props.editKitName} history={this.props.history} deleteKit={this.props.deleteKit} key={currentKit.id} kit={currentKit} />
+                                )
+                            })
+                    }
+                </section>
+            ) : (
+                    <section className="styleKits">
+                        <div className="centeredUhOh" style={{textAlign: "center"}}>Uh oh! Looks like you haven't created any kits.<br />Try saving your current kit to get started!</div>
+                    </section>
+                )
         )
     }
 }
